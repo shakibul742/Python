53fb7224be737afc7488ea6b433a7a376e044e8f
Md. Shakibul Islam <shakibulislam74254285@gmail.com>
2025-11-01 22:58:53 +0600

    some example

---
+++ py19.py
@@ -0,0 +1,11 @@
+# For loop example
+for num in range(1, 6):
+    print("Number:", num)
+
+print("\n\n")
+
+# While loop example
+count = 3
+while count > 0:
+    print("Countdown:", count)
+    count -= 1
